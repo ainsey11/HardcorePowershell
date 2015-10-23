ec1a2bf00bb9bcfb783cd171e99ab77dfdf6d741
Robert Ainsworth <robert@ainsey11.com>
2015-10-24 00:28:24 +0100

    Initial Commit
    Added readme and v1 of the hardcore powershell functions

---
+++ Hardcore Powershell.ps1
@@ -0,0 +1,49 @@
+﻿$wrongPassword = Get-Random # ignore
+$username = whoami # ignore
+$servername = "a11-dc-2" # enter dc name here or another servers name
+$modulename = "ActiveDirectory" # this is the module that needs to be loaded for the action to change
+
+    
+function Lock-ADAccount{
+    Write-Host -ForegroundColor Green "You failed, so I'm now locking your AD Account out.Nice try though!"
+do {
+    $password = ConvertTo-SecureString $wrongPassword -AsPlainText -Force
+    $cred= New-Object System.Management.Automation.PSCredential ($username, $password )
+    Enter-PSSession -ComputerName $servername -Credential $cred -ErrorAction SilentlyContinue
+    
+    
+}
+until ((Get-ADUser -Identity $env:USERNAME -Properties LockedOut).LockedOut)
+ 
+
+                        }
+
+
+function Start-HardcoreMode {
+#params
+    Param(
+   [int] $hardcoreness
+    )
+#checking module loaded
+    $ADLoaded = Get-Module -Name $modulename
+        if ($ADLoaded -like "$modulename")
+        { $moduleloaded = "yes" }
+        else { $moduleloaded = "no"}
+       
+
+if ($hardcoreness -eq "1" -and $moduleloaded -eq "yes")
+    {
+    Write-Host -ForegroundColor Green "Enabling hardcore mode 1, be careful!"
+    $ExecutionContext.InvokeCommand.CommandNotFoundAction = {Lock-ADAccount}
+    }
+if ($hardcoreness -eq "2" -and $moduleloaded -eq "yes")
+    {
+    Write-Host -ForegroundColor Green "Enabling hardcore mode 2, be careful!"
+    $ExecutionContext.InvokeCommand.CommandNotFoundAction = {Disable-ADAccount -Identity $env:USERNAME}
+    }
+if ($moduleloaded -eq "no") {
+    Write-Host -ForegroundColor Red "$modulename is not loaded, please load and run me again"
+    }
+}
+
+Start-HardcoreMode 2
---
+++ README.md
@@ -0,0 +1,30 @@
+# Ainsey11's Hardcore Powershell Function
+
+This awesome bit of code makes using powershell more hardcore, if you get a command wrong it can do two things.
+1. Lock your AD Account
+2. Disable your AD Account
+
+# Configuration
+
+you can add it to your global profile in powershell or run it when you want to, it's up to you!
+when using the function run it as so:
+
+Start-HardcoreMode -Hardcoreness <level>
+
+the levels are :
+
+1 - locks AD account
+2 - Disables AD account
+
+The initial idea came from a colleague and friend (jfrmilner) who suggested that I made the code from here : http://deploymentresearch.com/Research/Post/390/Enabling-PowerShell-Hardcore-Mode 
+lock or disable AD Accounts.  I took the idea and made this glorious function!
+you can see his blog over at : https://jfrmilner.wordpress.com/
+
+##### Disclaimer
+I have created this script for people to have a laugh and challenge themselves. I will not be held responsible for any data loss or damage caused by this script.Always test scrips in a non-production environment and make sure that you're happy before running on a live system. This is a standard disclaimer I use on all my code.
+
+Thanks!!
+
+Ainsey11
+
+https://ainsey11.com
